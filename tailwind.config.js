e28b77e8d78ad9a209e75302b2290ad228b53e47
Ridwan2638 <sanniridwanolamide@gmail.com>
2024-09-22 13:01:54 +0100

    added all the components of the web page and tailwind screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    screens:{
+      mobile:'320px',
+      tablet:'768px',
+      palmtop:'1152',
+      laptop:'1440px',
+      desktop:'1920px'
+    },
     extend: {},
   },
   plugins: [],
