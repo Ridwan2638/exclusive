2328b3fc3c559322c73705059023d227dc3f4c70
Ridwan2638 <sanniridwanolamide@gmail.com>
2024-12-09 14:29:27 +0100

    update the package,main.jsx and yan.lock

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const client_ID = "984446154658-ofm8pn4g67381346evek8pl8jsvabelj.apps.googleusercontent.com"
 
 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <GoogleOAuthProvider clientId={client_ID}>
+
     <App />
+    </GoogleOAuthProvider>
+    
     </BrowserRouter>
     
   </React.StrictMode>
